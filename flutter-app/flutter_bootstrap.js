6cdf94f74db908866cc52542c8af1d94165554f8
Zelda <ze@zelda.sh>
2026-04-06 16:14:50 +0200

    Fix prompt to authenticate on startup

--- flutter-app/flutter_bootstrap.js
+++ flutter-app/flutter_bootstrap.js
@@ -48,6 +48,6 @@ _flutter.buildConfig = { "engineRevision": "425cfb54d01a9472b3e81d9e76fd63a4a44c
 
 _flutter.loader.load({
     serviceWorkerSettings: {
-        serviceWorkerVersion: "178515748" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+        serviceWorkerVersion: "3278865472" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
     }
 });
